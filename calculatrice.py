2397758fc5569b32f0f835e0478b8e50a4854357
Ayaee <afabre@artfx.fr>
2022-02-03 15:57:52 +0100

    v1_calcula

---
+++ calculatrice.py
@@ -0,0 +1,11 @@
+p_nbr = int(input("Entrez un premier nombre : "))
+d_nbr = int(input("Entrez un deuxième nombre : "))
+
+résultat = f"Le résultat de l'addition de {p_nbr} avec {d_nbr} est égal à {p_nbr + d_nbr}"
+
+print(résultat)
+
+'''
+Possibilité si envie me prend de faire en sorte que si l'utilisateur n'écrit pas un chiffre cela annule le résultat et redemande à l'utilisateur
+de marquer un chiffre (avec une boucle)
+'''
